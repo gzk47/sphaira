b6b1af595983994dc6bcda75523295ed0bfbc6ca
ITotalJustice <47043333+ITotalJustice@users.noreply.github.com>
2025-08-31 04:27:53 +0100

    Revert "Clean up python (#212)"
    This reverts commit 0a8bc018705795eb5fb84924d58899cbf42f5caf.

--- tools/usb_common.py
+++ tools/usb_common.py
@@ -1,96 +1,50 @@
 import struct
-from time import sleep
-from usb.core
+import usb.core
 import usb.util
+import time
 
-SPLASH = """                                           
-      :@@@@@@@@@@@@@@@@@@@@@@@@@@:      
-      #@                        @#      
-      #@                        @#      
-      #@                        @#      
-      #@    @@@@@@    @@@@@@    @#      
-      #@    @@@@@@    @@@@@@    @#      
-      #@    @@@@@@    @@@@@@    @#      
-      #@                        @#      
-      #@                        @#      
-      #@                        @#      
-    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   
-   @          @%         @@@@@@@@@@@@  
-  #@  *@@@@*  @%         @@@@@@@@@@@@@  
-  #@ @@@@@@@@ @%         @@@@@@@@@@@@@  
-  #@ @@@@@@@@ @%         @@@@@@@@@@@@@  
-  #@  *@@@@*  @%         @@@@@@@@@@@@@  
-  #@          @%         @@@@@@@@@@@@@  
-  #@          @%         @@@@@@@@@@@@@  
-  #@          @%         @@@@@@@@@@@@@  
-  #@          @%         @@@@@@@@@@@@@  
-  #@          @%         @@@@@@@@@@@@@  
-  #@          @%         @@@@@=--=@@@@  
-  #@          @%         @@@-      -@@  
-  #@          @%         @@@        @@  
-  #@          @%         @@@-      -@@  
-  =@@         @%         @@@@@=--=@@@@  
-   =@@        @%         @@@@@@@@@@@@   
-     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                      
-"""
+# magic number (SPH0) for the script and switch.
+MAGIC = 0x53504830
 
-class USB_ENUM:
-    # (SPH0) for the script and Switch
-    MAGIC = 0x53504830 
+# commands
+CMD_QUIT = 0
+CMD_OPEN = 1
+CMD_EXPORT = 1
 
-    # Commands
-    CMD_QUIT = 0
-    CMD_OPEN = 1
-    CMD_EXPORT = 1
+# results
+RESULT_OK = 0
+RESULT_ERROR = 1
 
-    # Result Codes
-    RESULT_OK = 0
-    RESULT_ERROR = 1
+# flags
+FLAG_NONE = 0
+FLAG_STREAM = 1 << 0
 
-    # Flags
-    FLAG_NONE = 0
-    FLAG_STREAM = 1 << 0
-
-    # Switch Vendor / Product ID
-    VENDOR_ID = 0x057E
-    PRODUCT_ID = 0x3000
-
-    ENABLE_ZLT = 0
-
-
-def find_switch() -> object | None:
-    return usb.core.find(
-        idVendor=USB_ENUM.VENDOR_ID,
-        idProduct=USB_ENUM.PRODUCT_ID
-    )
+# disabled, see usbds.cpp usbDsEndpoint_SetZlt
+ENABLE_ZLT = 0
 
 class Usb:
     def __init__(self):
-        self._out_ep = None
-        self._in_ep = None
-        self._packet_size = 0
-        self._packet_index = 0
+        self.__out_ep = None
+        self.__in_ep = None
+        self.__packet_size = 0
 
     def wait_for_connect(self) -> None:
-        print(SPLASH)
-        print("Waiting for Switch...", end="")
+        print("waiting for switch")
 
         dev = None
-        while dev is None:
-            if (dev := find_switch()):
-                break
-            print(".", end="")
-            sleep(0.5)
+        while (dev is None):
+            dev = usb.core.find(idVendor=0x057E, idProduct=0x3000)
+            if (dev is None):
+                time.sleep(0.5)
 
-        print("Found the Switch!\n")
+        print("found the switch!\n")
         cfg = None
 
-        print("Getting configuration...")
         try:
             cfg = dev.get_active_configuration()
-            print("Found active config")
+            print("found active config")
         except usb.core.USBError:
-            print("No currently active config")
+            print("no currently active config")
             cfg = None
 
         if cfg is None:
@@ -100,36 +54,26 @@ class Usb:
 
         is_out_ep = lambda ep: usb.util.endpoint_direction(ep.bEndpointAddress) == usb.util.ENDPOINT_OUT
         is_in_ep = lambda ep: usb.util.endpoint_direction(ep.bEndpointAddress) == usb.util.ENDPOINT_IN
-        self._out_ep = usb.util.find_descriptor(cfg[(0,0)], custom_match=is_out_ep)
-        self._in_ep = usb.util.find_descriptor(cfg[(0,0)], custom_match=is_in_ep)
+        self.__out_ep = usb.util.find_descriptor(cfg[(0,0)], custom_match=is_out_ep)
+        self.__in_ep = usb.util.find_descriptor(cfg[(0,0)], custom_match=is_in_ep)
+        assert self.__out_ep is not None
+        assert self.__in_ep is not None
 
-        if not self._out_ep:
-            raise ValueError("Failed to get USB OUT address")
-        if not self._in_ep:
-            raise ValueError("Failed to get USB IN address")
-
-        print(f"iManufacturer: {dev.manufacturer} \
-            iProduct: {dev.product} \
-            iSerialNumber: {dev.serial_number}")
-
-        print(f"bcdUSB: {hex(dev.bcdUSB)} \
-            bMaxPacketSize0: {dev.bMaxPacketSize0}")
-        self._packet_size = 1 << dev.bMaxPacketSize0
+        print("iManufacturer: {} iProduct: {} iSerialNumber: {}".format(dev.manufacturer, dev.product, dev.serial_number))
+        print("bcdUSB: {} bMaxPacketSize0: {}".format(hex(dev.bcdUSB), dev.bMaxPacketSize0))
+        self.__packet_size = 1 << dev.bMaxPacketSize0
 
     def read(self, size: int, timeout: int = 0) -> bytes:
-        if (ENABLE_ZLT and size and not (size % self._packet_size)):
+        if (ENABLE_ZLT and size and (size % self.__packet_size) == 0):
             size += 1
-        return self._in_ep.read(size, timeout)
+        return self.__in_ep.read(size, timeout)
 
     def write(self, buf: bytes, timeout: int = 0) -> int:
-        packet = self._packet_index
-        self._packet_index += 1
-        # Todo, implement packet index long
-        return self._out_ep.write(data=buf, timeout=timeout)
+        return self.__out_ep.write(data=buf, timeout=timeout)
 
     def get_send_header(self) -> tuple[int, int, int]:
         header = self.read(16)
-        magic, arg2, arg3, arg4 = struct.unpack('<IIII', header)
+        [magic, arg2, arg3, arg4] = struct.unpack('<IIII', header)
 
         if magic != MAGIC:
             raise Exception("Unexpected magic {}".format(magic))
--- tools/usb_export.py
+++ tools/usb_export.py
@@ -1,40 +1,34 @@
 import crc32c
 import sys
 import os
-from usb_common import Usb, USB_ENUM as UE, SPLASH
+from pathlib import Path
+from usb_common import *
 
 def get_file_name(usb: Usb, name_length: int) -> str:
     return bytes(usb.read(name_length)).decode('utf-8')
 
-def create_file_folder(root: os.PathLike, target: os.PathLike) -> bool, os.PathLike:
-    """
-    Creates a recursive folder structure at a given location
-    Returns a boolean indicating if it already exists and the absolute path 
-    """
-    path = os.abspath(os.path.join(root, target))
-    parent = os.path.dirname(path)
-    if not (os.path.exists(parent)):
-        os.makedirs(path)
-        print(f"Created folder {path}")
-    else:
-        print(f"Parent folder already exists {path}")
-    exists_already = os.path.exists(path)
-    return exists_already, path
+def create_file_folder(root: Path, file_path: Path) -> Path:
+    # todo: check if it already exists.
+    full_path = Path(root + "/" + file_path)
+    full_path.parent.mkdir(exist_ok=True, parents=True)
+    print("created folder")
 
-def wait_for_input(usb: Usb, path: os.PathLike) -> None:
+    return full_path
+
+def wait_for_input(usb: Usb, path: Path) -> None:
     print("now waiting for intput\n")
 
-    with open(path, "wb") as f:
-        print(f"Opened file {path}")
+    with open(path, "wb") as file:
+        print("opened file {}".format(path))
 
         while True:
             [off, size, crc32c_want] = usb.get_send_data_header()
 
             # todo: this isn't needed really.
-            usb.send_result(UE.RESULT_OK)
+            usb.send_result(RESULT_OK)
 
             # check if we should finish now.
-            if off == 0 and size == 0:
+            if (off == 0 and size == 0):
                 break
 
             # read the buffer and calculate the crc32c.
@@ -42,31 +36,33 @@ def wait_for_input(usb: Usb, path: os.PathLike) -> None:
             crc32c_got = crc32c.crc32c(buf)
 
             # validate the crc32c matches.
-            if crc32c_want != crc32c_got:
-                usb.send_result(UE.RESULT_ERROR)
+            if (crc32c_want != crc32c_got):
+                usb.send_result(RESULT_ERROR)
                 continue
 
             try:
-                f.seek(off)
-                f.write(buf)
-                usb.send_result(UE.RESULT_OK)
+                file.seek(off)
+                file.write(buf)
+                usb.send_result(RESULT_OK)
             except BlockingIOError as e:
                 print("Error: failed to write: {} at: {} size: {} error: {}".format(e.filename, off, size, str(e)))
-                usb.send_result(UE.RESULT_ERROR)
+                usb.send_result(RESULT_ERROR)
 
 if __name__ == '__main__':
-    print(SPLASH)
+    print("hello world")
 
-    if not len(args) == 2:
-        print("Pass root path as argument.")
+    # check which mode the user has selected.
+    args = len(sys.argv)
+    if (args != 2):
+        print("pass the folder path")
         sys.exit(1)
 
     root_path = sys.argv[1]
 
     if (not os.path.isdir(root_path)):
-        raise ValueError('')
+        raise ValueError('must be a dir!')
 
-    usb = Usb()
+    usb: Usb = Usb()
 
     try:
         # get usb endpoints.
@@ -76,21 +72,21 @@ if __name__ == '__main__':
         while True:
             [cmd, arg3, arg4] = usb.get_send_header()
 
-            if (cmd == UE.CMD_QUIT):
-                usb.send_result(UE.RESULT_OK)
+            if (cmd == CMD_QUIT):
+                usb.send_result(RESULT_OK)
                 break
-            elif (cmd == UE.CMD_EXPORT):
-                usb.send_result(UE.RESULT_OK)
+            elif (cmd == CMD_EXPORT):
+                usb.send_result(RESULT_OK)
 
                 # todo: handle and return errors here.
                 file_name = get_file_name(usb, arg3)
-                exists_already, full_path = create_file_folder(root_path, file_name)
-                usb.send_result(UE.RESULT_OK)
+                full_path = create_file_folder(root_path, file_name)
+                usb.send_result(RESULT_OK)
 
                 wait_for_input(usb, full_path)
             else:
-                usb.send_result(UE.RESULT_ERROR)
+                usb.send_result(RESULT_ERROR)
                 break
 
-    except Exception as e:
-        print(f"An exception occurred - {e} ")
+    except Exception as inst:
+        print("An exception occurred " + str(inst))
